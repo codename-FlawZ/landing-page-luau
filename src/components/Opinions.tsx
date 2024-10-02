9abb1d4cc059f28edd24b3e04aa9e08f1b23c24c
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2024-10-02 18:58:48 -0300

    fix - 3

--- src/components/Opinions.tsx
+++ src/components/Opinions.tsx
@@ -1,4 +1,8 @@
+import { opinions } from "../constants";
+import { twMerge } from "tailwind-merge";
+
 export default function Opinions() {
+
   return (
 	<section 
 	 className="
@@ -14,71 +18,22 @@ export default function Opinions() {
 			">
 				Opinião dos alunos
 			</h1>
-			<div 
-			 className="
-			  opinion-cards-container
-			">
-				<div 
-				 className="
-				  opinion-cards
-				">
-					<div className="flex flex-col justify-between">
-					 <h1 className="opinion-username">Danielly Brito - 3°A</h1>
-					 <h1 className="opinion-username-category">Vice-Representante de sala</h1>
-					</div>
-					<div className="flex flex-col justify-between">
-					 <p className="user-opinion-text">Achei uma ideia muito maneira e diferenciada, Vejo que pode ser um evento que podemos descansar e não sair da contexto escolar em um dia.</p>
-					</div>
-				</div>
-				<div 
-				 className="
-				  opinion-cards
-				  max-sm:hidden
-				">
-					<div className="flex flex-col justify-between">
-					 <h1 className="opinion-username">Laura Sales - 3°A</h1>
-					 <h1 className="opinion-username-category">Estudante</h1>
-					</div>
-					<div className="flex flex-col justify-between">
-					 <p className="user-opinion-text">Acredito que a proposta é ótima! Inovadora, real e interessante. Nunca houve nada do tipo na escola de comemoração para a entrada de estações!</p>
-					</div>
-				</div>
-				<div 
-				 className="
-				  opinion-cards
-				  max-sm:hidden
-				">
-					<div className="flex flex-col justify-between">
-					 <h1 className="opinion-username">Melissa Viana - 3°A</h1>
-					 <h1 className="opinion-username-category">Estudante</h1>
-					</div>
-					<div className="flex flex-col justify-between">
-					 <p className="user-opinion-text">Bom, a ideia é boa e pode ser algo diferente para a escola, já que nunca teve um baile na escola. Com isso podemos ver o quão interessante possa ser ter um baile de primavera.</p>
-					</div>
-				</div>
-				<div 
-				 className="
-				  opinion-cards
-				">
-					<div className="flex flex-col justify-between">
-					 <h1 className="opinion-username">Henrique Almeida - 3°A</h1>
-					 <h1 className="opinion-username-category">Representante de sala</h1>
-					</div>
-					<div className="flex flex-col justify-between">
-					 <p className="user-opinion-text">Eu achei essa ideia extremamente interessante por conta de promover a igualdade e ser algo super importante para a conscientização das campanhas existentes em outubro. Além de ser uma maneira divertida e dinâmica</p>
-					</div>
-				</div>
-				<div 
-				 className="
-				  opinion-cards
-				">
-					<div className="flex flex-col justify-between">
-					 <h1 className="opinion-username">Aline Santos - 3°A</h1>
-					 <h1 className="opinion-username-category">Cordenadora de comunicação suplente</h1>
-					</div>
-					<div className="flex text-start justify-between">
-					 <p className="user-opinion-text">Uma idéia incrível, onde todos poderíamos curtir, descansar em conjunto, sem deixar de aprender sobre coisas importantes</p>
-					</div>
+			<p
+			 className="section-description"
+			>
+				Veja abaixo a opinião de alguns alunos
+			</p>
+			<div className="flex justify-center gap-6">
+				<div className={twMerge("flex md:flex-row max-sm:flex-col md:gap-8 md:pt-8 max-sm:gap-5 max-sm:pt-5 md:[mask-image:linear-gradient(to_left,transparent,black_25%,black_75%,transparent)] max-sm:[mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]")}>
+					{opinions?.map(({ text, name, role }) => (
+						<div className="card" key={name || role}>
+							<div className="flex flex-col">
+								<div className="opinion-username">{name}</div>
+								<div className="opinion-username-category">{role}</div>
+							</div>
+							<div className="user-opinion-text">{text}</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const opinions = [
+	{
+		text: "Achei uma ideia muito maneira e diferenciada, Vejo que pode ser um evento que podemos descansar e não sair da contexto escolar em um dia.",
+		name: "Danielly Brito - 3°A",
+		role: "Vice-Representante de Sala",
+	},
+	{
+		text: "Acredito que a proposta é ótima! Inovadora, real e interessante. Nunca houve nada do tipo na escola de comemoração para a entrada de estações!",
+		name: "Laura Sales - 3°A",
+		role: "Estudante",
+	},
+	{
+		text: "Bom, a ideia é boa e pode ser algo diferente para a escola, já que nunca teve um baile na escola. Com isso podemos ver o quão interessante possa ser ter um baile de primavera.",
+		name: "Melissa Viana - 3°A",
+		role: "Estudante",
+	},
+	{
+		text: "Eu achei essa ideia extremamente interessante por conta de promover a igualdade e ser algo super importante para a conscientização das campanhas existentes em outubro. Além de ser uma maneira divertida e dinâmica",
+		name: "Henrique Almeida - 3°A",
+		role: "Representante de sala",
+	},
+	{
+		text: "Uma idéia incrível, onde todos poderíamos curtir, descansar em conjunto, sem deixar de aprender sobre coisas importantes",
+		name: "Aline Santos - 3°A",
+		role: "Cordenadora de comunicação suplente",
+	},
+];
